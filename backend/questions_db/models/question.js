3c9d8d2304996ced3dad0fa03b22a3f9af7c908b
imraj singh <92265647+imrajsinghsandhu@users.noreply.github.com>
2023-09-15 12:39:04 +0800

    Provide joi middleware verification for question

--- backend/questions_db/models/question.js
+++ backend/questions_db/models/question.js
@@ -16,7 +16,39 @@ const question = new mongoose.model('Question', new mongoose.Schema({
         type: String,
         required:true,
         minlength:50,
-        maxlength:1000
+        maxlength:3000
     },
-    
+    categories : {
+        type: Array,
+        required:true,
+        minlength:5,
+        maxlength:200
+    },
+    question_link: {
+        type: String,
+        required:true,
+        minlength:10,
+        maxlength:200,
+    },
+    solution_link: {
+        type: String,
+        required:true,
+        minlength:10,
+        maxlength:200
+    }
 }))
+
+const validateQuestion = (question) => {
+    const schema = Joi.object({
+        title:Joi.string().min(5).max(500).required(),
+        description:Joi.string().min(50).max(3000).required(),
+        categories:Joi.array().min(5).max(200).required(),
+        question_link:Joi.string().min(10).max(200).required(),
+        solution_link:Joi.string().min(10).max(200).required(),
+    });
+
+    return schema.validate(question);
+}
+
+exports.Question = question;
+exports.validate = validateQuestion;
